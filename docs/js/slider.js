be6f88f43f2747573990820a91b2e90abc266d0f
DenisRyssen <122613016+DenisRyssen@users.noreply.github.com>
2023-08-02 22:09:29 +0300

    Add files via upload

---
+++ docs/js/slider.js
@@ -0,0 +1,33 @@
+const gallary = new Swiper('.gallary', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 500,
+    slidesPerView: 1,
+
+    effect: 'cards',
+    cardsEffect: {
+      perSlideOffset: 10,
+      rotate: true,
+      perSlideRotate: 0,
+    },
+    //slidesPerView: 1.2,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+     // prevtEl: ".btn-prev",
+    },
+  
+    // Navigation arrows
+     navigation: {
+      nextEl: '.btn-next',
+      //prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
